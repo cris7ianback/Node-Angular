1c64b0f59d2b3bd7d5cbc40520a4f60b0d9f0128
cris7ianback <cristian.viragor@gmail.com>
2022-03-29 15:29:27 -0300

    v.1.29.2
    Fin de Jornda

--- front-end/frontend/src/app/components/login/login.component.ts
+++ front-end/frontend/src/app/components/login/login.component.ts
@@ -1,30 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validator, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { fromEventPattern } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
+
+  public loginForm!: FormGroup;
+
+  private emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
+  createFormGroup() {
+    return new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.minLength(5),Validators.pattern(this.emailPattern)]),
+      password: new FormControl('', [Validators.required,Validators.minLength(5)])
+      //mensaje: new FormControl('', [Validators.required])
+    })
+  }
 
   user = {
     email: '',
     password: ''
   }
+  formBuilder: any;
+  
 
-  constructor( private authService: AuthService,
-               private router: Router) { }
+  constructor(private authService: AuthService,
+              private router: Router
+  ) {
+    this.loginForm = this.createFormGroup();
+   }
 
-  ngOnInit(){
+  ngOnInit(): void {
   }
 
   login() {
 
-//    console.log('prueba')
+    //    console.log('prueba')
     this.authService.login(this.user)
-    .subscribe(       
+      .subscribe(
         res => {
           console.log(res);
           localStorage.setItem('token', res.token);
@@ -33,7 +55,11 @@ export class LoginComponent implements OnInit {
         },
         err => console.log(err)
       )
-      
+
+  }
+
+  OnResetForm(): void{
+    this.loginForm.reset();
   }
 
 
